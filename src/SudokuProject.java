dfdc882669d409bac5365c451202c88d02b570a9
Clayton <claytoncblevins@gmail.com>
2022-02-22 18:31:15 -0500

    Update SudokuProject.java
    Changed process from isEmpty method to manually checking all inputs. Also made puzzle a global variable.

--- src/SudokuProject.java
+++ src/SudokuProject.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 
 public class SudokuProject{
-    private int row, col;
+    int[][] puzzle;
 
     //Constructor
     public SudokuProject(){
-        row = -1;
-        col = -1;
         int[][] sudokuPuzzle = {
             {3, 0, 6, 5, 0, 8, 4, 0, 0},
             {5, 2, 0, 0, 0, 0, 0, 0, 0},
@@ -19,31 +17,38 @@ public class SudokuProject{
             {0, 0, 5, 2, 0, 6, 3, 0, 0}
         };
 
-        sudokuSolver(sudokuPuzzle);
-            for(int[] x : sudokuPuzzle)
-                System.out.println(Arrays.toString(x)); 
-       // }else{
-           // System.out.println("No Solution");
-        //}
+        puzzle = new int[9][9];
+        puzzle = sudokuPuzzle;
+
+        for(int[] x : puzzle)
+        if(sudokuSolver()){
+            System.out.println(Arrays.toString(x)); 
+        }else{
+           System.out.println("No Solution");
+        }
     }
 
-    public boolean sudokuSolver(int[][] puzzle){
-        if(!isEmpty(puzzle))
-            return true;
-        
-        for(int i = 1; i < 10; i++)
-            if(isValid(puzzle, i)){
-                puzzle[row][col] = i;
-                if(sudokuSolver(puzzle))
-                    return true;
-                puzzle[row][col] = 0;
+    public boolean sudokuSolver(){
+        for(int r = 0; r < puzzle[0].length; r++){
+            for(int c = 0; c < puzzle.length; c++){
+                if(puzzle[r][c] == 0){
+                    for(int i = 1; i < 10; i++){
+                        if(isValid(r, c, i)){
+                            puzzle[r][c] = i;
+                            if(sudokuSolver())
+                                return true;
+                            puzzle[r][c] = 0;
+                        }
+                    }
+                return false;
+                }
             }
-
-        return false;
+        }
+        return true;
     }
 
     //Check if input is in row
-    public boolean rowValid(int[][] puzzle, int input){
+    public boolean rowValid(int row, int input){
         for(int c = 0; c < 9; c++)
             if(puzzle[row][c] == input)
                 return true;
@@ -52,7 +57,7 @@ public class SudokuProject{
     }
 
     //Check if input is in column
-    public boolean colValid(int[][] puzzle, int input){
+    public boolean colValid(int col, int input){
         for(int r = 0; r < 9; r++)
             if(puzzle[r][col] == input)
                 return true;
@@ -61,7 +66,7 @@ public class SudokuProject{
     }
 
     //Checks the 9 3x3 boxes for 'input'
-    public boolean boxValid(int[][] puzzle, int input){
+    public boolean boxValid(int row, int col, int input){
         int r = row - row % 3;
         int c = col - col % 3;
 
@@ -76,21 +81,8 @@ public class SudokuProject{
     }
 
     //Checks for valid input
-    public boolean isValid(int[][] puzzle, int input){
-        return !rowValid(puzzle, input) && !colValid(puzzle, input) && !boxValid(puzzle, input);
-    }
-
-    //Finds empty spaces
-    public boolean isEmpty(int[][] puzzle){
-        for(int r = 0; r < puzzle[0].length; r++)
-            for(int c = 0; c < puzzle.length; c++)
-                if(puzzle[r][c] == 0){
-                    row = r;
-                    col = c;
-                    return true;
-                }
-                
-        return false;
+    public boolean isValid(int row, int col, int input){
+        return !rowValid(row, input) && !colValid(col, input) && !boxValid(row, col, input);
     }
 
     //Main Method
